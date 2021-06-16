8c1041fd86606edead1a97c5bdbb22a0619d947c
hasanaydin0 <84991976+hasanaydin0@users.noreply.github.com>
2021-06-16 19:02:36 +0300

    timer added

--- app/src/main/java/com/hasanaydin/quizapp/second/SecondActivity.kt
+++ app/src/main/java/com/hasanaydin/quizapp/second/SecondActivity.kt
@@ -3,13 +3,16 @@ package com.hasanaydin.quizapp.second
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import com.hasanaydin.quizapp.QuestionsData
 import com.hasanaydin.quizapp.R
 import com.hasanaydin.quizapp.ResultActivity
 import com.hasanaydin.quizapp.databinding.ActivityMainBinding
 import com.hasanaydin.quizapp.databinding.ActivitySecondBinding
+import com.hasanaydin.quizapp.main.MainActivity
 import com.hasanaydin.quizapp.main.MainViewModel
 import com.hasanaydin.quizapp.question1
 
@@ -27,8 +30,23 @@ class SecondActivity : AppCompatActivity() {
         setContentView(view)
         viewModel = ViewModelProvider(this).get(SecondViewModel::class.java)
 
+        // Timer
+
+        object : CountDownTimer(10300,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                binding.time.text = "Süre: "+ millisUntilFinished/1000
+            }
+
+            override fun onFinish() {
+                
+            }
+
+        }.start()
+
+
+
         var uname = intent.getStringExtra(EXTRA_MESSAGE)
-        binding.userName2.text = "Başarılar, $uname :)"
+        binding.userName2.text = "Başarılar, $uname"
 
         binding.question.text = question1.question
         binding.answer1.text = question1.answer1
@@ -54,7 +72,7 @@ class SecondActivity : AppCompatActivity() {
         }
 
         binding.answer3.setOnClickListener {
-            var message = "Tebrikler, Doğru Cevap"
+            var message = "Tebrikler, Doğru Cevap :)"
             var intent = Intent(this,ResultActivity::class.java).apply {
                 putExtra(EXTRA_MESSAGE,message)
             }
