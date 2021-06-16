914b800e50f0ba85a41dfe39bc4048bf15912ab3
hasanaydin0 <84991976+hasanaydin0@users.noreply.github.com>
2021-06-16 16:26:32 +0300

    data class added

---
+++ app/src/main/java/com/hasanaydin/quizapp/QuestionsData.kt
@@ -0,0 +1,23 @@
+package com.hasanaydin.quizapp
+
+import android.content.Intent
+
+data class QuestionsData(
+    val question: String,
+    val answer1: String,
+    val answer2: String,
+    val answer3: String,
+    val answer4: String
+)
+
+val question1 = QuestionsData("Bir ayda kaç gün vardır?","10","20","30","40")
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/hasanaydin/quizapp/second/SecondActivity.kt
+++ app/src/main/java/com/hasanaydin/quizapp/second/SecondActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import androidx.lifecycle.ViewModelProvider
+import com.hasanaydin.quizapp.QuestionsData
 import com.hasanaydin.quizapp.R
 import com.hasanaydin.quizapp.databinding.ActivityMainBinding
 import com.hasanaydin.quizapp.databinding.ActivitySecondBinding
 import com.hasanaydin.quizapp.main.MainViewModel
+import com.hasanaydin.quizapp.question1
 
 class SecondActivity : AppCompatActivity() {
 
@@ -25,5 +27,11 @@ class SecondActivity : AppCompatActivity() {
 
         var uname = intent.getStringExtra(EXTRA_MESSAGE)
         binding.userName2.text = "Başarılar, $uname :)"
+
+        binding.question.text = question1.question
+        binding.answer1.text = question1.answer1
+        binding.answer2.text = question1.answer2
+        binding.answer3.text = question1.answer3
+        binding.answer4.text = question1.answer4
     }
 }
